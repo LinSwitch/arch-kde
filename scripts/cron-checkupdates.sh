579be8c89c4df7b258e01144a5d0fec82b61266e
linswitch <225473498+LinSwitch@users.noreply.github.com>
2025-11-23 17:58:36 +0000

    Create cron-checkupdates.sh

---
+++ scripts/cron-checkupdates.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Уведомление о новых обновлениях Arch (checkupdates)
+
+# --- переменные для cron ---
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+
+# --- считаем пакеты ---
+mapfile -t updates < <(checkupdates 2>/dev/null)
+count=${#updates[@]}
+
+# --- выводим только если есть ---
+if (( count > 0 )); then
+    notify-send -u critical -i system-software-update \
+                "Arch 🚀" \
+                "$count новых обновлений доступно
+                ${updates[*]:0:3}$( ((count>3)) && echo " …и ещё $((count-3))" )"
+fi
